45b4bd6cbc238c779e5afeb4280acdea353fe952
owinter <olivier.winter@hotmail.fr>
2024-10-17 15:18:21 +0100

    add test jsonable - move test folder inside package for pip installed

--- iblutil/tests/test_io.py
+++ iblutil/tests/test_io.py
@@ -220,5 +220,28 @@ class TestsJsonable(unittest.TestCase):
         os.unlink(self.tfile.name)
 
 
+class TestLoadTaskData(unittest.TestCase):
+    def test_load_task_jsonable(self):
+        jsonable_file = Path(__file__).parent.joinpath('fixtures', 'task_data_short.jsonable')
+        trials_table, bpod_data = jsonable.load_task_jsonable(jsonable_file)
+        assert trials_table.shape[0] == 2
+        assert len(bpod_data) == 2
+
+    def test_load_task_jsonable_partial(self):
+        jsonable_file = Path(__file__).parent.joinpath('fixtures', 'task_data_short.jsonable')
+        with open(jsonable_file) as fp:
+            fp.readline()
+            offset = fp.tell()
+        trials_table, bpod_data = jsonable.load_task_jsonable(jsonable_file, offset=offset)
+
+        trials_table_full, bpod_data_full = jsonable.load_task_jsonable(jsonable_file)
+        for c in trials_table.columns:
+            if not np.isnan(trials_table[c][0]):
+                np.testing.assert_equal(trials_table_full[c].values[-1], trials_table[c][0])
+
+        assert bpod_data_full[-1] == bpod_data[0]
+
+
+
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
