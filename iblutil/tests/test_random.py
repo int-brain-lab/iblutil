79debdd397094cba0f9f11b174cc66ca93cd0dce
owinter <olivier.winter@hotmail.fr>
2025-07-04 16:18:33 +0100

    add test

--- iblutil/tests/test_random.py
+++ iblutil/tests/test_random.py
@@ -1,5 +1,6 @@
 import unittest
 
+import hashlib
 import iblutil.random
 
 
@@ -7,3 +8,7 @@ class TestRandom(unittest.TestCase):
     def test_silly_name(self):
         name = iblutil.random.sillyname()
         assert len(name) > 3, 'Generated name is empty'
+
+    def test_from_hash(self):
+        h = hashlib.md5('6a317f9b-587a-4524-8528-677744a8134f.sdfi'.encode())
+        self.assertEqual('powerful-yellowgreen-lemming', iblutil.random.name_from_hash(h))
